e3efe582ad85d7faff9ce32acb79dfd8284c4332
vlad veretennikov <veretennikov.vld@gmail.com>
2020-07-27 16:12:13 +0300

    first virsion

--- script/index.js
+++ script/index.js
@@ -3,20 +3,28 @@ $(document).ready(function () {
     $(this).toggleClass('active');
     $('body').toggleClass('lock');
     $('.header__menu').toggleClass('active');
+    $('.header__menu').on('click', function () {
+      $(this).removeClass('active');
+    });
+    $('.header__menu-item').on('click', function (event) {
+      event.stopPropagation();
+    });
   });
 
-  $('[data-filter]').on('click', function (event) {
+  const filterBtn = $('[data-filter]');
+  const filterItem = $('[data-category]');
+  filterBtn.on('click', function (event) {
     event.preventDefault();
-    $('[data-filter]').removeClass('active');
+    filterBtn.removeClass('active');
     $(this).addClass('active');
-    let filterCategory = $(this).data('filter');
+    let dataValueFilterBtn = $(this).attr('data-filter');
 
-    if (filterCategory == 'all') {
-      $('[data-category').show(0);
+    if (dataValueFilterBtn == 'all') {
+      filterItem.show(0);
     } else {
-      $('[data-category]').each(function () {
-        let itemCategory = $(this).data('category');
-        if (itemCategory != filterCategory) {
+      filterItem.each(function () {
+        let dataValueFilterItem = $(this).attr('data-category');
+        if (dataValueFilterBtn != dataValueFilterItem) {
           $(this).hide(0);
         } else {
           $(this).show(0);
@@ -24,5 +32,37 @@ $(document).ready(function () {
       })
     }
   });
+  $('.m-gallery__img-slider').slick({
+    arrows: false,
+    dots: true,
+  });
 
+  const modalCall = $('[data-modal]');
+  const modalClose = $('[data-close]');
+  modalCall.on('click', function (event) {
+    event.preventDefault;
+    let modalId = $(this).attr('data-modal');
+    $(modalId).addClass('show');
+    $('body').addClass('lock');
+  });
+
+  modalClose.on('click', function (event) {
+    event.preventDefault;
+    let modalParent = $(this).parents('.modal');
+    modalParent.removeClass('show');
+    $('body').removeClass('lock');
+  });
+
+  $('.modal').on('click', function (event) {
+    event.preventDefault();
+    $(this).removeClass('show');
+    $('body').removeClass('lock');
+  });
+
+  $('.m-gallery__unit').on('click', function (event) {
+    event.stopPropagation();
+  });
+  $('.m-resume__item').on('click', function (event) {
+    event.stopPropagation();
+  });
 });
